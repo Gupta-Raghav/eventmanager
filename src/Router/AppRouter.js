5acc040185d5b0b5a4628db48743c479b965208a
raghuweer23 <42888633+raghuweer23@users.noreply.github.com>
2021-03-05 10:38:16 +0530

    initial commit
    Hello Karan
    
    Co-Authored-By: Karan Mannan <drkaranmannan@gmail.com>

---
+++ src/Router/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import createHistory from 'history/createBrowserHistory';
+import {Router, Switch,Route} from 'react-router-dom';
+import Landing from '../components/Landing Page/Landing';
+import Dashboard from '../components/Dashboard/dashboard'
+import NotFoundPage from '../components/NotFoundPage';
+export const history = createHistory();
+const AppRouter = () => (
+    <Router history={history}>
+        <div>
+            <Switch>
+            <Route path="/" component={Landing} exact={true}/>
+            <Route path='/home' component={Dashboard} />
+            <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </Router>
+)
+export default AppRouter;
---
+++ src/components/Dashboard/dashboard.js
@@ -0,0 +1,12 @@
+import { Dashboard } from '@material-ui/icons'
+import React from 'react';
+import './dashboard.css';
+
+const EventsDashboard = () => {
+    return (
+        <div className="home">
+            <h1>This is our home page</h1>
+        </div>
+    )
+}
+export default EventsDashboard;
