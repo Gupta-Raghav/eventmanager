785144aa845e8c03b1cdafcfcab9b3173bcfca28
karan <karan@radicali.com>
2021-03-06 11:20:46 +0530

    add grids to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import {
   makeStyles,
   Button,
@@ -12,7 +12,7 @@ import {
   Toolbar,
 } from '@material-ui/core';
 import logo from './mujlogo.png';
-import './navbar.css'
+import './navbar.css';
 import UserProvider, { UserContext } from '../providers/UserProvider';
 import { Redirect } from 'react-router-dom';
 import { auth } from '../../firebase';
@@ -23,50 +23,99 @@ const useStyles = makeStyles(() => ({
   menuButton: {
     marginRight: '16px',
   },
-  img:{
-      marginTop: '4px',
-      paddingTop: '4px',
+  img: {
+    marginTop: '4px',
+    paddingTop: '4px',
     height: '3em',
     width: '14em',
   },
   title: {
-      color: 'black',
+    color: 'black',
     flexGrow: 1,
   },
 }));
 const startLogOut = () => {
-  auth.signOut().then(()=> {
-    console.log('logged out')
-  }).catch((error) => {
-    console.log(error.message)
-  })
-}
+  auth
+    .signOut()
+    .then(() => {
+      console.log('logged out');
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
 export default function Navbar() {
-    const classes = useStyles();
-    const user = useContext(UserContext);
-    const [redirect, setRedirect] = useState(null);
-    
-    useEffect(() => {
-      if (!user) {
-        setRedirect("/");
-      }
-    }, [user]);
-    if (redirect) {
-      <Redirect to={redirect} />;
+  const classes = useStyles();
+  const user = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
+
+  useEffect(() => {
+    if (!user) {
+      setRedirect('/');
     }
-    return (
-        <div className={classes.root}>
-            <AppBar position='static' className='navbar'>
-                <Toolbar>
-                    <div className={classes.menuButton}>
-                        <img src={logo} className={classes.img} alt='logo'/>
-                    </div>
-                    {/* <Typography variant='h5' className={classes.title}>
-                        MUJ EVENTS BRRRR
-                        </Typography> */}
-                        <Button color='inherit' className={classes.button} onClick={startLogOut}>Logout</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+  }, [user]);
+  if (redirect) {
+    <Redirect to={redirect} />;
+  }
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' className='navbar'>
+        <Toolbar>
+          <Grid container row justifyContent='space-between'>
+            <Grid item xs>
+              <div className={classes.menuButton}>
+                <img src={logo} className={classes.img} alt='logo' />
+              </div>
+            </Grid>
+            <Grid item xs />
+            <Grid item xs>
+              <Grid container direction='column'>
+                <Grid item xs={2} />
+                <Grid item xs>
+                  <Grid container justifyContent='center' alignItems='center'>
+                    <Grid item xs>
+                      <Button
+                        color='inherit'
+                        className={classes.button}
+                        onClick={startLogOut}
+                      >
+                        Logout
+                      </Button>
+                    </Grid>
+                    <Grid item xs>
+                      <Button
+                        color='inherit'
+                        className={classes.button}
+                        onClick={startLogOut}
+                      >
+                        Logout
+                      </Button>
+                    </Grid>
+                    <Grid item xs>
+                      <Button
+                        color='inherit'
+                        className={classes.button}
+                        onClick={startLogOut}
+                      >
+                        Logout
+                      </Button>
+                    </Grid>
+                    <Grid item xs>
+                      <Button
+                        color='inherit'
+                        className={classes.button}
+                        onClick={startLogOut}
+                      >
+                        Logout
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
