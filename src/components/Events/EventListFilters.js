b769c524b9c1a1f7c4b4c8d07253fb173ec7d50f
raghuweer23 <42888633+raghuweer23@users.noreply.github.com>
2021-06-13 19:58:35 +0530

    event list filter

--- src/components/Events/EventListFilters.js
+++ src/components/Events/EventListFilters.js
@@ -5,6 +5,11 @@ import { useHistory } from 'react-router-dom';
 import Selector from './components/Selector';
 import { useStyles } from '@material-ui/pickers/views/Calendar/SlideTransition';
 import eventsFilter from '../../Selectors/events';
+<<<<<<< Updated upstream
+=======
+import {setTextFilter, sortByDate, setStartDate, setEndDate,setType} from '../../actions/filters'
+import MenuItem from '@material-ui/core/MenuItem';
+>>>>>>> Stashed changes
 import {
   setTextFilter,
   sortByDate,
@@ -33,6 +38,7 @@ const EventsListFilters = () => {
   // const [startDate, setStartDate] = useState(new Date('2014-08-18T21:11:54'));
   // const [endDate, setEndDate] = useState(new Date('2014-08-18T21:11:54'));
   const [searchItem, setSearchItem] = useState('');
+  const [type, settype] = useState('');
   const filteredEvents = eventsFilter(
     events,
     searchItem,
@@ -40,6 +46,7 @@ const EventsListFilters = () => {
     // startDate,
     // endDate
   );
+<<<<<<< Updated upstream
 
   const onStartDateChange = ({ startDate }) => {
     dispatch(setStartDate(startDate));
@@ -101,6 +108,25 @@ const EventsListFilters = () => {
               <form
                 className={`${classes.container} ${classes.date}`}
                 noValidate
+=======
+  
+  
+    const onStartDateChange =({startDate}) =>{
+      dispatch(setStartDate(startDate));
+    }
+    const onEndDateChange =({endDate}) => {
+      dispatch(setEndDate(endDate))
+    }
+    const types = ['Technical', 'Cultural', 'Department','curricular', 'social'];
+    return (
+        <div>
+             <Grid item>
+              <FormControl
+                fullWidth
+                // className={classes.margin}
+                // variant='outlined'
+                variant='filled'
+>>>>>>> Stashed changes
               >
                 <TextField
                   id='date'
@@ -116,6 +142,7 @@ const EventsListFilters = () => {
                 />
               </form>
             </Grid>
+<<<<<<< Updated upstream
             <Grid item>
               <form
                 className={`${classes.container} ${classes.date}`}
@@ -141,4 +168,88 @@ const EventsListFilters = () => {
     </div>
   );
 };
+=======
+            {/* <Filters /> */}
+            <Grid
+              // className={classes.margin}
+              container
+              justify='space-between'
+            >
+              <Grid item>
+                <Grid container>
+                  <Grid item>
+                  <InputLabel id="demo-mutiple-name-label">Club </InputLabel>
+                    <Select
+                      searchItem={searchItem}
+                      list={filters}
+                      placeholder='Club'
+                    />
+                  </Grid>
+                  <Grid item>
+                  <InputLabel id="demo-mutiple-name-label">Type</InputLabel>
+                    
+                    <Select
+                      // TODO: set-up separate states for separate selectors
+                      searchItem={searchItem}
+                      value= {filters.type}
+                      onChange={(e)=> dispatch(setType(e.target.value))}
+                      setSearchItem={setSearchItem}
+                      labelWidth={20}
+                    >
+                      {types.map((type) => (
+                        <MenuItem key={type} value={type}>
+                          {type}
+                        </MenuItem>
+                      ))}
+                      </Select>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container>
+                  <Grid item>
+                    <form
+                      className={`${classes.container} ${classes.date}`}
+                      noValidate
+                    >
+                      <TextField
+                        id='date'
+                        label='Start Date'
+                        type='date'
+                        defaultValue='2017-05-24'
+                        className={classes.textField}
+                        value ={filters.startDate}
+                        onChange={onStartDateChange}
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                      />
+                    </form>
+                  </Grid>
+                  <Grid item>
+                    <form
+                      className={`${classes.container} ${classes.date}`}
+                      noValidate
+                    >
+                      <TextField
+                        id='date'
+                        label='End Date'
+                        type='date'
+                        defaultValue='2017-05-24'
+                        className={classes.textField}
+                        value ={filters.endDate}
+                        onChange={onEndDateChange}
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                      />
+                    </form>
+                  </Grid>
+                  </Grid>
+              </Grid>
+            </Grid>        
+        </div>
+    )
+}
+>>>>>>> Stashed changes
 export default EventsListFilters;
